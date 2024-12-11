8f580708f93696c041f7fdd27b24a50d9d852bdf
kesatria H1 <128112182+AexonSec@users.noreply.github.com>
2024-12-11 14:02:52 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,192 +1,110 @@
 #!/bin/bash
-sudo apt-get -y update
-sudo apt-get -y upgrade
 
+# Update system and install dependencies
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install -y libcurl4-openssl-dev libssl-dev jq ruby-full libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev libffi-dev python3-dev python3-pip git rename xargs awscli nmap snapd
 
-sudo apt-get install -y libcurl4-openssl-dev
-sudo apt-get install -y libssl-dev
-sudo apt-get install -y jq
-sudo apt-get install -y ruby-full
-sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
-sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
-sudo apt-get install -y python-setuptools
-sudo apt-get install -y libldns-dev
-sudo apt-get install -y python3-pip
-sudo apt-get install -y python-pip
-sudo apt-get install -y python-dnspython
-sudo apt-get install -y git
-sudo apt-get install -y rename
-sudo apt-get install -y xargs
-
-echo "installing bash_profile aliases from recon_profile"
-git clone https://github.com/nahamsec/recon_profile.git
-cd recon_profile
-cat bash_profile >> ~/.bash_profile
-source ~/.bash_profile
-cd ~/tools/
-echo "done"
-
-
-
-#install go
-if [[ -z "$GOPATH" ]];then
-echo "It looks like go is not installed, would you like to install it now"
-PS3="Please select an option : "
-choices=("yes" "no")
-select choice in "${choices[@]}"; do
-        case $choice in
-                yes)
-
-					echo "Installing Golang"
-					wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
-					sudo tar -xvf go1.13.4.linux-amd64.tar.gz
-					sudo mv go /usr/local
-					export GOROOT=/usr/local/go
-					export GOPATH=$HOME/go
-					export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-					echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
-					echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile			
-					echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile	
-					source ~/.bash_profile
-					sleep 1
-					break
-					;;
-				no)
-					echo "Please install go and rerun this script"
-					echo "Aborting installation..."
-					exit 1
-					;;
-	esac	
-done
+# Install Go
+if ! command -v go &> /dev/null; then
+    echo "Installing Go..."
+    wget https://dl.google.com/go/go1.21.0.linux-amd64.tar.gz
+    tar -xvf go1.21.0.linux-amd64.tar.gz
+    chmod +x go/bin/go
+    sudo mv go/bin/go /usr/local/bin/go
+    export GOROOT=/usr/local/go
+    export GOPATH=$HOME/go
+    export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+    echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+    echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+    echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
+    source ~/.bashrc
+    rm go1.21.0.linux-amd64.tar.gz
+    echo "Go installed."
 fi
 
+# Create tools directory
+mkdir -p ~/tools
+cd ~/tools
 
-#Don't forget to set up AWS credentials!
-echo "Don't forget to set up AWS credentials!"
-apt install -y awscli
-echo "Don't forget to set up AWS credentials!"
+# Function to check and install tools
+install_tool() {
+    local name=$1
+    local repo=$2
+    if [ ! -d "$name" ]; then
+        echo "Installing $name..."
+        git clone $repo
+        echo "$name installed."
+    else
+        echo "$name is already installed."
+    fi
+}
 
+# Install tools
+install_tool "Aquatone" "https://github.com/michenriksen/aquatone.git"
+install_tool "JSParser" "https://github.com/nahamsec/JSParser.git"
+cd JSParser && sudo python3 setup.py install && cd ~/tools
 
+install_tool "Sublist3r" "https://github.com/aboul3la/Sublist3r.git"
+cd Sublist3r && pip3 install -r requirements.txt && cd ~/tools
 
-#create a tools folder in ~/
-mkdir ~/tools
-cd ~/tools/
+install_tool "wpscan" "https://github.com/wpscanteam/wpscan.git"
+cd wpscan && sudo gem install bundler && bundle install --without test && cd ~/tools
 
-#install aquatone
-echo "Installing Aquatone"
-go get github.com/michenriksen/aquatone
-echo "done"
+install_tool "dirsearch" "https://github.com/maurosoria/dirsearch.git"
+install_tool "lazys3" "https://github.com/nahamsec/lazys3.git"
+install_tool "virtual host discovery" "https://github.com/jobertabma/virtual-host-discovery.git"
+install_tool "sqlmap" "https://github.com/sqlmapproject/sqlmap.git"
+install_tool "knock.py" "https://github.com/guelfoweb/knock.git"
+install_tool "lazyrecon" "https://github.com/nahamsec/lazyrecon.git"
+install_tool "massdns" "https://github.com/blechschmidt/massdns.git"
+cd massdns && make && cd ~/tools
 
-#install chromium
-echo "Installing Chromium"
-sudo snap install chromium
-echo "done"
+install_tool "asnlookup" "https://github.com/yassineaboukir/asnlookup.git"
+cd asnlookup && pip3 install -r requirements.txt && cd ~/tools
 
-echo "installing JSParser"
-git clone https://github.com/nahamsec/JSParser.git
-cd JSParser*
-sudo python setup.py install
-cd ~/tools/
-echo "done"
+# Install Go-based tools
+if ! command -v httprobe &> /dev/null; then
+    go install github.com/tomnomnom/httprobe@latest
+    echo "httprobe installed."
+else
+    echo "httprobe is already installed."
+fi
 
-echo "installing Sublist3r"
-git clone https://github.com/aboul3la/Sublist3r.git
-cd Sublist3r*
-pip install -r requirements.txt
-cd ~/tools/
-echo "done"
+if ! command -v unfurl &> /dev/null; then
+    go install github.com/tomnomnom/unfurl@latest
+    echo "unfurl installed."
+else
+    echo "unfurl is already installed."
+fi
 
+if ! command -v waybackurls &> /dev/null; then
+    go install github.com/tomnomnom/waybackurls@latest
+    echo "waybackurls installed."
+else
+    echo "waybackurls is already installed."
+fi
 
-echo "installing teh_s3_bucketeers"
-git clone https://github.com/tomdev/teh_s3_bucketeers.git
-cd ~/tools/
-echo "done"
+install_tool "crtndstry" "https://github.com/nahamsec/crtndstry.git"
 
+# Download Seclists
+echo "Downloading SecLists..."
+if [ ! -d "SecLists" ]; then
+    git clone https://github.com/danielmiessler/SecLists.git
+    cd SecLists/Discovery/DNS
+    sed '$d' dns-Jhaddix.txt > clean-jhaddix-dns.txt
+    cd ~/tools
+else
+    echo "SecLists is already downloaded."
+fi
 
-echo "installing wpscan"
-git clone https://github.com/wpscanteam/wpscan.git
-cd wpscan*
-sudo gem install bundler && bundle install --without test
-cd ~/tools/
-echo "done"
+# Install Chromium
+if ! command -v chromium &> /dev/null; then
+    sudo snap install chromium
+    echo "Chromium installed."
+else
+    echo "Chromium is already installed."
+fi
 
-echo "installing dirsearch"
-git clone https://github.com/maurosoria/dirsearch.git
-cd ~/tools/
-echo "done"
-
-
-echo "installing lazys3"
-git clone https://github.com/nahamsec/lazys3.git
-cd ~/tools/
-echo "done"
-
-echo "installing virtual host discovery"
-git clone https://github.com/jobertabma/virtual-host-discovery.git
-cd ~/tools/
-echo "done"
-
-
-echo "installing sqlmap"
-git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
-cd ~/tools/
-echo "done"
-
-echo "installing knock.py"
-git clone https://github.com/guelfoweb/knock.git
-cd ~/tools/
-echo "done"
-
-echo "installing lazyrecon"
-git clone https://github.com/nahamsec/lazyrecon.git
-cd ~/tools/
-echo "done"
-
-echo "installing nmap"
-sudo apt-get install -y nmap
-echo "done"
-
-echo "installing massdns"
-git clone https://github.com/blechschmidt/massdns.git
-cd ~/tools/massdns
-make
-cd ~/tools/
-echo "done"
-
-echo "installing asnlookup"
-git clone https://github.com/yassineaboukir/asnlookup.git
-cd ~/tools/asnlookup
-pip install -r requirements.txt
-cd ~/tools/
-echo "done"
-
-echo "installing httprobe"
-go get -u github.com/tomnomnom/httprobe 
-echo "done"
-
-echo "installing unfurl"
-go get -u github.com/tomnomnom/unfurl 
-echo "done"
-
-echo "installing waybackurls"
-go get github.com/tomnomnom/waybackurls
-echo "done"
-
-echo "installing crtndstry"
-git clone https://github.com/nahamsec/crtndstry.git
-echo "done"
-
-echo "downloading Seclists"
-cd ~/tools/
-git clone https://github.com/danielmiessler/SecLists.git
-cd ~/tools/SecLists/Discovery/DNS/
-##THIS FILE BREAKS MASSDNS AND NEEDS TO BE CLEANED
-cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
-cd ~/tools/
-echo "done"
-
-
-
-echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
-ls -la
-echo "One last time: don't forget to set up AWS credentials in ~/.aws/!"
+# Completion message
+echo -e "\n\nSetup completed! Tools are installed in ~/tools."
+echo "Don't forget to set up AWS credentials in ~/.aws/!"
